3e109c2d50d3a3517c5be906815bc7bc61e1b976
watakak <155397402+watakak@users.noreply.github.com>
2024-07-12 20:41:45 +0200

    FreshGUI 0.8.6

--- freshgui.py
+++ freshgui.py
@@ -4,16 +4,21 @@ from PIL import Image, ImageTk
 from ctypes import windll
 import tkinter as tk
 
-#FRESHGUI MODULE VERSION 0.4.2
+#FRESHGUI MODULE VERSION 0.8.6
 
 projectName = 'FreshGUI'
-projectVersion = '0.4.2'
+projectVersion = '0.8.6'
 projectAuthor = 'watakak'
 githubRepo = f'https://github.com/{projectAuthor}/{projectName}'
 
 root = tk.Tk()
 buttons = []
+labels = []
 dict = {}
+
+root.title('FreshGUI Window')
+root.geometry('400x300')
+root.iconbitmap('assets/FGUI.ico')
 
 def create(name='FreshGUI Window', size='400x300', icon='assets/FGUI.ico', resizable=True,
            transparency=False, focus=False, fullscreen=False):
@@ -37,14 +42,46 @@ def create(name='FreshGUI Window', size='400x300', icon='assets/FGUI.ico', resiz
         window_blur = windll.user32.GetForegroundWindow()
         blur(window_blur, hexColor='#12121240')
 
-def text(var='textVariable', text='FreshText', font=None, size=12, background=False, cornerRadius=5,
-         position='center', up=0, down=0, left=0, right=0):
+def textWidget(bg=None, fg=None, font='Consolas', size=16, cursor=None, insertBlink=True, state='enabled',
+               insertWidth=None, spacing=None, bordersWidth=2, scroll=False):
+    bg = themeBG
+    fg = activeWidgetsBG
+
+    if insertBlink == True:
+        insertBlink = 600
+    elif insertBlink == False:
+        insertBlink = 0
+    else:
+        insertBlink = insertBlink
+
+    if scroll == True:
+        print(True)
+
+    text_area = tk.Text(wrap="word")
+    text_area.config(background=bg, foreground=fg, font=(font, size), cursor=cursor, relief='flat',
+                     insertofftime=insertBlink, insertontime=insertBlink, selectbackground=selectBG,
+                     selectforeground=fg, insertbackground=fg, insertwidth=insertWidth, spacing3=spacing,
+                     padx=bordersWidth, pady=bordersWidth)
+    text_area.pack(expand=True, fill="both")
+
+def text(var='textVariable', text='FreshText', font=None, size=12, color='themed',
+         background=False, cornerRadius=5, position='center', up=0, down=0, left=0, right=0):
     if background == True:
         textColor = themeBG
-        backgroundState = widgetsBG
+        backgroundState = mainTextColor
     elif background == False:
-        textColor = widgetsBG
+        textColor = mainTextColor
         backgroundState = 'transparent'
+
+    if color == 'themed':
+        textColor = mainTextColor
+    elif color == 'gray':
+        textColor = '#868686'
+
+    #Fonts
+    #SF Pro Text Bold - Bold
+    #SF Pro Text - Regular
+    #SF Pro Text Regular - Thin
 
     sizex = size / 0.3
     sizey = size / 0.6
@@ -59,8 +96,8 @@ def text(var='textVariable', text='FreshText', font=None, size=12, background=Fa
 
 def button(var='buttonVariable', text='FreshButton', command=None, size=10, position='center',
            cursor='hand2', shape='rectangle', color=None, font=None, fontSize=12, image=None,
-           imageResize=True, imagePlace='left', cornerRadius=7, borders=False,
-           up=0, down=0, left=0, right=0, state='working'):
+           imageResize=True, imagePlace='left', imageSize=10, cornerRadius=7, borders=False,
+           up=0, down=0, left=0, right=0, active=True, width=False, height=False):
 
     if shape == 'rectangle':
         sizex = size / 0.06
@@ -72,16 +109,24 @@ def button(var='buttonVariable', text='FreshButton', command=None, size=10, posi
         if logs:
             print('FGUI ERROR: Shape must be "rectangle" or "square"')
 
+    if width and height:
+        sizex = width
+        sizey = height
+
     if image:
         image = tk.PhotoImage(file=image)
 
-        if imageResize == True:
-            reduction_factor = max(1, 2 ** ((image.width() * image.height()) // (120 * 120)))
+        if imageResize:
+            max_width, max_height = sizex / 1.5, sizey / 1.5
+            scale_factor = max(image.width() / max_width, image.height() / max_height, 1)
+            reduction_factor = int(scale_factor)
+            reduction_factor = max(1, reduction_factor)
             image = image.subsample(reduction_factor)
 
-    if state == 'working':
+    if active == True:
         state = 'normal'
-    elif state == 'disabled':
+    else:
+        state = 'disabled'
         cursor = 'arrow'
 
     if color == None:
@@ -113,7 +158,7 @@ def button(var='buttonVariable', text='FreshButton', command=None, size=10, posi
 
 def imageButton(var='buttonVariable', image=None, command=None, size=None, position='center',
                 cursor='hand2', up=0, down=0, left=0, right=0, filtering='smooth',
-                state='working'):
+                active=True):
     if image == None:
         image = 'assets/FGUI.ico'
         size = 15
@@ -137,9 +182,10 @@ def imageButton(var='buttonVariable', image=None, command=None, size=None, posit
 
     photo = ImageTk.PhotoImage(image)
 
-    if state == 'working':
+    if active == True:
         state = 'normal'
-    elif state == 'disabled':
+    else:
+        state = 'disabled'
         cursor = 'arrow'
 
     button = tk.Button(root, image=photo, width=imageSize, height=imageSize, cursor=cursor,
@@ -151,6 +197,45 @@ def imageButton(var='buttonVariable', image=None, command=None, size=None, posit
 
     buttons.append((button, position, {'up': up, 'down': down, 'left': left, 'right': right}))
     return button
+
+def input(var='inputVariable', text='FreshInput', font=None, size=16, state='normal',
+          show=None, position='center', up=0, down=0, left=0, right=0, cornerRadius=5, bordersWidth=2):
+
+    entry = ctk.CTkEntry(root, font=(font, size), fg_color=mainTextColor,
+                         text_color=themeBG, placeholder_text=text, placeholder_text_color='#868686',
+                         corner_radius=cornerRadius, border_width=bordersWidth, state=state, show=show)
+
+    dict[var] = entry
+
+    buttons.append((entry, position, {'up': up, 'down': down, 'left': left, 'right': right}))
+    return entry
+
+def checkBox(var='checkBoxVariable', text='FreshCheckBox', position='center', up=0, down=0, left=0,
+             right=0, command=None, size=10, cornerRadius=6, borderWidth=2):
+    size = size * 2.5
+
+    checkbox = ctk.CTkCheckBox(root, text=text, onvalue='on', offvalue='off', command=command,
+                               checkbox_width=size, checkbox_height=size, corner_radius=cornerRadius,
+                               border_width=borderWidth, fg_color=widgetsBG, border_color=widgetsBG,
+                               hover_color=activeWidgetsBG, text_color=widgetsBG, checkmark_color=themeBG)
+
+    dict[var] = checkbox
+
+    buttons.append((checkbox, position, {'up': up, 'down': down, 'left': left, 'right': right}))
+    return checkbox
+
+def checkBoxState(var='checkBoxVariable'):
+    # Assuming dict is accessible here
+    if var in dict:
+        checkbox = dict[var]
+        state = checkbox.get()  # Assuming your checkbox widget has a get() method
+        if state == 'on':
+            return True
+        else:
+            return False
+    else:
+        print('Checkbox variable', var, 'not found.')
+        return None
 
 def updateButtonPositions():
     new_width = root.winfo_width()
@@ -201,24 +286,39 @@ def menu(var='menuVariable', options=1):
 
     root.config(menu=dict[var])
 
-def settings(theme='Light', debug=False, language='English'):
-    global themeBG, widgetsBG, activeWidgetsBG, borderColor, textColor
+def scene(scene='scene'):
+    print(scene)
+
+def settings(theme='light', debug=False, language='English'):
+    global themeBG, widgetsBG, activeWidgetsBG, borderColor, textColor, selectBG, mainTextColor
     global logs
 
     logs = debug
 
-    if theme == 'Dark':
+    if theme == 'dark':
         themeBG = '#1F1F1F'
         widgetsBG = '#E0E0E0'
+        mainTextColor = '#E0E0E0'
         activeWidgetsBG = '#BDBDBD'
         borderColor = '#868686'
         textColor = '#1F1F1F'
-    elif theme == 'Light':
+        selectBG = '#191919'
+    elif theme == 'light':
         themeBG = '#E0E0E0'
         widgetsBG = '#2E2E2E'
+        mainTextColor = '#2E2E2E'
         activeWidgetsBG = '#1F1F1F'
         borderColor = '#868686'
         textColor = '#E0E0E0'
+        selectBG = '#C7C7C7'
+    elif theme == 'default':
+        themeBG = '#FFFFFF'
+        widgetsBG = '#9E9E9E'
+        mainTextColor = '#000000'
+        activeWidgetsBG = '#757575'
+        borderColor = '#868686'
+        textColor = '#000000'
+        selectBG = '#C7C7C7'
     else:
         print("This Theme doesn't exists!")
 
